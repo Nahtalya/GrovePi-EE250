cd252b0a88f2eac6f1017be5308fd46788e9e5ae
Natalia <nh43953@usc.edu>
2022-09-13 13:13:08 -0700

    changed the ports and stuff

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
 	
 	// TODO: Fill out the server ip and port
 	std::string server_ip = "20.171.56.97"; //got through ipconfig /all
-	std::string server_port = "20"; //grabbed through the tcp server port of things 
+	std::string server_port = "8080"; //grabbed through the tcp server port of things 
 
 	int opt = 1;
 	int client_fd = -1;
